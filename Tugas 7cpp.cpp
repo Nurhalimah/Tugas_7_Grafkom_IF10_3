0329e29792ef98c4044c3b99c64cd90f7f396310
Anggi Mondera Amino <sanceszarnino@yahoo.com>
2012-05-21 22:56:50 -0700

    Update Tugas 7cpp.cpp

--- Tugas 7cpp.cpp
+++ Tugas 7cpp.cpp
@@ -134,7 +134,7 @@ glutInit(&argc,argv);
 glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 glutInitWindowSize(640,480);
 glutInitWindowPosition(100,100);
-glutCreateWindow("Tugas 7 Grafika Komputer");
+glutCreateWindow("Tugas 7 Grafika Komputer Kelompok 3 IF10");
 glutDisplayFunc(displayObject);
 glEnable(GL_LIGHTING);
 glEnable(GL_LIGHT0);
